37d63d5c7df96b82b7709050f8de3affc5f97517
Taina De Alves <tainadesouzalves@gmail.com>
2024-02-29 22:30:59 -0300

    começamos a formatar o js do api do ibge

---
+++ json/localidades/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", async function(){
+
+    const listaMunicipio = document.getElementById("municipios-list");
+
+    const resposta = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/municipios?orderBy=nome');
+
+    const dados = await resposta.json();
+
+    dados.forEach(function(municipio) {
+
+        const li = document.createElement('li');
+
+        li.textContent = municipio.nome;
+
+        listaMunicipio.appendChild(li);
+        
+    });
+}
+
+
+)
